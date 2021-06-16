139ce53a9ec9d3798cc7afe1819e1e1a3c948bd5
Pablo Nicolas Sanchez <pablokuko@gmail.com>
2021-06-16 19:59:00 -0300

    agregando Alta Prenda con combo de tipo Prenda

---
+++ WebApplication1/WebApplication1/Controllers/PrendasController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using WebApplication1.Servicios;
+
+namespace WebApplication1.Controllers
+{
+    public class PrendasController : Controller
+    {
+        private ILocalServicio _localServicio;
+        private IPrendaServicio _prendaServicio;
+        private ITipoPrendaServicio _tipoPrendaServicio;
+        public PrendasController()
+        {
+            VestimentasDBContext dbContext = new VestimentasDBContext();
+            _localServicio = new LocalServicio(dbContext);
+            _prendaServicio = new PrendaServicio(dbContext);
+            _tipoPrendaServicio = new TipoPrendaServicio(dbContext);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Alta()
+        {
+            ViewBag.TodasTipoPrendas = _tipoPrendaServicio.ObtenerTodos();
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Alta(Prendum prenda)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.TodasPrendas = _prendaServicio.ObtenerTodos();
+                return View(prenda);
+            }
+
+            _prendaServicio.Alta(prenda);
+            return View(prenda);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Servicios/TIpoPrendaServicio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Servicios
+{
+    public class TipoPrendaServicio : ITipoPrendaServicio
+    {
+        private VestimentasDBContext _dbContext;
+        public TipoPrendaServicio(VestimentasDBContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public TipoPrendum ObtenerPorId(int id)
+        {
+            return _dbContext.TipoPrenda
+                .FirstOrDefault(o=> o.IdTipoPrenda == id);
+        }
+
+        public List<TipoPrendum> ObtenerTodos()
+        {
+            return _dbContext.TipoPrenda.ToList();
+        }
+
+        public void Alta(TipoPrendum tipoPrenda)
+        {
+            _dbContext.TipoPrenda.Add(tipoPrenda);
+            _dbContext.SaveChanges();
+        }
+
+        public void Borrar(TipoPrendum tipoPrenda)
+        {
+            _dbContext.TipoPrenda.Remove(tipoPrenda);
+            _dbContext.SaveChanges();
+        }
+    }
+}
